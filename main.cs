9e50e8bb5a3607b705a8dde202e3ad87902480e1
kawa1sou <ne2erm0re@gmail.com>
2020-04-12 00:20:31 +0300

    updated

--- main.cs
+++ main.cs
@@ -4,24 +4,22 @@ namespace ConsoleApp1
 {
     public static class Convertions
     {
-         public static string ToAlternativeCase(string word)
-             {
-                int wordLength = word.Length;
-                char[] wordarray = word.ToCharArray();
-                char[] result = new char[wordLength];
-                for (int i = 0; i < wordLength; i++)
-                {
-                    bool isTrue = Char.IsUpper(wordarray[i]);
-                    if (isTrue == true)
-                    {
-                        result[i] = Char.ToLower(wordarray[i]);
-                    }
-                    else
-                        result[i] = Char.ToUpper(wordarray[i]);
-                }
-                string word = new string(result);
-                return word;
-                   throw new NotImplementedException();
+        public static string ToAlternativeCase(string word)
+        {
+            int wordLength = word.Length;
+            char[] wordarray = word.ToCharArray();
+            char[] result = new char[wordLength];
+            for (int i = 0; i < wordLength; i++)
+            {
+                bool isTrue = Char.IsUpper(wordarray[i]);
+                if (isTrue == true)
+                    result[i] = Char.ToLower(wordarray[i]);
+                else
+                    result[i] = Char.ToUpper(wordarray[i]);
+            }
+            string changedWord = new string(result);
+            return changedWord;
+            throw new NotImplementedException();
         }
     }
     class Program
@@ -29,8 +27,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string word = Console.ReadLine();
-            Convertions.ToAlternativeCase(word);
-            Console.WriteLine(word);
+            string uResult = Convertions.ToAlternativeCase(word);
+            Console.WriteLine(uResult);
             Console.ReadKey();
         }
     }
